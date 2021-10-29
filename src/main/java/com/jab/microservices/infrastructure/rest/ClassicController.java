e4d7339457e1e8fb95c36d272d01db7ee9029fda
Juan Antonio Breña Moral <bren@juanantonio.info>
2021-10-29 12:29:43 +0200

    Testing Native Docker Image generation for Java 17

--- src/main/java/com/jab/microservices/infrastructure/rest/ClassicController.java
+++ src/main/java/com/jab/microservices/infrastructure/rest/ClassicController.java
@@ -5,7 +5,11 @@ import com.jab.microservices.application.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/classic",
